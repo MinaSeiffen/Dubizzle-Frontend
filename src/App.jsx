bcea67ccb1eeda748f808271507d28ffbaf15d96
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-19 14:10:54 +0200

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, Router, RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./AppLayout";
 import Property from "./Pages/Property/Property";
-import { Provider } from "react-redux";
-import StroeConfig from "./Store/Store";
 import Favorite from "./Pages/Favorite/Favorite";
 import { SellPage } from "./Pages/Sell/SellPage";
 import { SellFormPage } from "./Pages/SellForm/SellFormPage";
-import { MenuSelectionProvider } from "./Context/MenuSelectionContext";
 import { ProductDetails } from "./Pages/ProductDetails/ProductDetails";
 import SubCategoryProducts from "./Pages/SubCategoryProducts/SubCategoryProducts";
 import SellerDetails from "./Pages/SellerDetails/SellerDetails";
@@ -16,9 +13,12 @@ import EditProfile from "./Pages/EditProfile/EditProfile";
 import useCheckingForToken from "./Hooks/useCheckingForToken";
 import { useEffect } from "react";
 import Chat from "./Pages/Chat/Chat";
-import { SocketContextProvider } from "./Context/SocketContext";
 import MyAds from "../src/Pages/MyAds/MyAds";
 import DropDownTogleDiv from "../src/Components/DropDwonTogleDiv/DropDownTogleDiv";
+import { Switch } from "antd";
+import Header from "./Components/HeaderComp/Header";
+import { Toaster } from "react-hot-toast";
+import { Footer } from "antd/es/layout/layout";
 
 function App() {
   const { getMyProfileFromToken, profile } = useCheckingForToken();
@@ -51,12 +51,29 @@ function App() {
 
   useEffect(() => {}, [profile]);
 
-  return (
-    <>
-      <RouterProvider router={routesPage}>
-        <AppLayout />
-      </RouterProvider>
-    </>
+  return  (
+    <Router>
+      <Header profile={profile}></Header>
+      <div className="md:mt-32 mt-30">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/search/:name?" component={SubCategoryProducts} />
+          <Route path="/searchforproperties" component={SubCategoryProducts} />
+          <Route path="/sell" component={SellPage} />
+          <Route path="/chat" component={Chat} />
+          <Route path="/sellform" component={SellFormPage} />
+          <Route path="/product-details/:id" component={ProductDetails} />
+          <Route path="/sellerADs/:id" component={SellerDetails} />
+          <Route path="/favorite" component={Favorite} />
+          <Route path="/EditProfile" component={EditProfile} />
+          <Route path="/property" component={Property} />
+          <Route path="/MyAds" component={MyAds} />
+          <Route path="/togle" component={DropDownTogleDiv} />
+        </Switch>
+        <Toaster />
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
