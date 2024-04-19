f99aebe8930571932455d4836f925f6f6395b395
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-19 12:31:54 +0200

    route update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="507185982911-p3ri6j87629qfv4tpj7mkebpi5onc4um.apps.googleusercontent.com">
-      <HashRouter>
+      <HashRouter basename='/'>
          <App />
       </HashRouter>
     </GoogleOAuthProvider>
