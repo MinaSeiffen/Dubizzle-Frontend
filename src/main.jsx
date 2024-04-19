4ccf59fb6d908cc588f6de4dc4c3302f3a92ab61
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-19 12:27:11 +0200

    routes update

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="507185982911-p3ri6j87629qfv4tpj7mkebpi5onc4um.apps.googleusercontent.com">
-      <BrowserRouter>
+      <HashRouter>
          <App />
-      </BrowserRouter>
+      </HashRouter>
     </GoogleOAuthProvider>
 
   </React.StrictMode>,
