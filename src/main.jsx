8475cfa3b71e15d2edae79b998ae777760f26754
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-19 12:41:32 +0200

    reset routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="507185982911-p3ri6j87629qfv4tpj7mkebpi5onc4um.apps.googleusercontent.com">
-      <HashRouter basename='/'>
-         <App />
-      </HashRouter>
+      <App />
     </GoogleOAuthProvider>
 
   </React.StrictMode>,
