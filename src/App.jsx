58dab5d6c6e7900e62981e1a0079a06738ef9f3a
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-19 14:28:02 +0200

    resetting

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,68 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./AppLayout";
 import Property from "./Pages/Property/Property";
+import { Provider } from "react-redux";
+import StroeConfig from "./Store/Store";
 import Favorite from "./Pages/Favorite/Favorite";
 import { SellPage } from "./Pages/Sell/SellPage";
 import { SellFormPage } from "./Pages/SellForm/SellFormPage";
+import { MenuSelectionProvider } from "./Context/MenuSelectionContext";
 import { ProductDetails } from "./Pages/ProductDetails/ProductDetails";
 import SubCategoryProducts from "./Pages/SubCategoryProducts/SubCategoryProducts";
 import SellerDetails from "./Pages/SellerDetails/SellerDetails";
 import EditProfile from "./Pages/EditProfile/EditProfile";
 import useCheckingForToken from "./Hooks/useCheckingForToken";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Chat from "./Pages/Chat/Chat";
-import MyAds from "../src/Pages/MyAds/MyAds";
-import DropDownTogleDiv from "../src/Components/DropDwonTogleDiv/DropDownTogleDiv";
-import { Toaster } from "react-hot-toast";
-import { Footer } from "antd/es/layout/layout";
-import { useDispatch } from "react-redux";
-import { likedProductAction } from "./Store/Slices/Favorites";
+import { SocketContextProvider } from "./Context/SocketContext";
+import MyAds from "../src/Pages/MyAds/MyAds"
+import DropDownTogleDiv from "../src/Components/DropDwonTogleDiv/DropDownTogleDiv"
 
 function App() {
   const { getMyProfileFromToken, profile } = useCheckingForToken();
-  const [token, setToken] = useState(null);
-  const getToken = () => {
-    const token = localStorage.getItem("jwt");
-    setToken(token);
-  };
-  const dispatch = useDispatch();
 
   useEffect(() => {
     getMyProfileFromToken();
-    getToken();
-    dispatch(likedProductAction());
-  }, [dispatch]);
+  }, []);
+
+  const routesPage = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "/search/:name?", element: <SubCategoryProducts /> },
+        { path: "/searchforproperties", element: <SubCategoryProducts /> },
+        { path: "/sell", element: <SellPage /> },
+        { path: "/chat", element: <Chat profile={profile} /> },
+        { path: "/sellform", element: <SellFormPage /> },
+        { path: "/product-details/:id", element: <ProductDetails /> },
+        { path: "/sellerADs/:id", element: <SellerDetails /> },
+        { path: "favorite", element: <Favorite /> },
+        { path: "/EditProfile", element: <EditProfile /> },
+        { path: "/property", element: <Property /> },
+        { path: "/MyAds", element: <MyAds /> },
+      ],
+    },
+    { path: "/togle", element:<DropDownTogleDiv togleV={true} /> },
+  ]);
+
+  useEffect(() => {}, [profile]);
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <AppLayout>
-              <Route index element={<Home />} />
-              <Route path="search/:name?" element={<SubCategoryProducts />} />
-              <Route path="searchforproperties" element={<SubCategoryProducts />} />
-              <Route path="sell" element={<SellPage />} />
-              <Route path="chat" element={<Chat profile={profile} />} />
-              <Route path="sellform" element={<SellFormPage />} />
-              <Route path="product-details/:id" element={<ProductDetails />} />
-              <Route path="sellerADs/:id" element={<SellerDetails />} />
-              <Route path="favorite" element={<Favorite />} />
-              <Route path="EditProfile" element={<EditProfile />} />
-              <Route path="property" element={<Property />} />
-              <Route path="MyAds" element={<MyAds />} />
-            </AppLayout>
-          }
-        />
-        <Route path="/togle" element={<DropDownTogleDiv togleV={true} />} />
-      </Routes>
-      <Toaster />
-      <Footer />
-    </Router>
+    <>
+      <MenuSelectionProvider>
+            <SocketContextProvider>
+        <Provider store={StroeConfig}>
+          <RouterProvider router={routesPage}>
+              <AppLayout />
+          </RouterProvider>
+        </Provider>
+            </SocketContextProvider>
+      </MenuSelectionProvider>
+    </>
   );
 }
 
